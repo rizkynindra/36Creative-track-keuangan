71cdd1a47a8c3c9fa3d07553ae9e5e6d79d98804
rizkynindra <rizkynindra@gmail.com>
2026-02-02 21:55:55 +0700

    add menu on the frontend

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
     handlers=[
-        logging.FileHandler("app.log"),
         logging.StreamHandler()
     ]
 )
