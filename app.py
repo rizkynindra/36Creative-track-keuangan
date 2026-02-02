77772295878eba45969e32f3bcaee4d9b6390120
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2026-02-02 16:04:24 +0700

    first commit

--- app.py
+++ app.py
@@ -202,4 +202,4 @@ def export_transactions():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3636, host='0.0.0.0')
